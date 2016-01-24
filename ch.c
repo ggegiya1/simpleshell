749291b200e657c5cf397baa1ab644dc00d9fc2a
Georgiy Gegiya <gegiya@gmail.com>
2016-01-24 13:39:33 -0500

    Refactor, handle input/output redirecting

--- ch.c
+++ ch.c
@@ -47,6 +47,13 @@
  * Prototypes
  * ############################################################################
  */
+ 
+struct Command{
+	char * command;
+	int input;
+	int output;
+	int status;
+};
 
 char * trim(char * s) 
 {
@@ -58,17 +65,17 @@ char * trim(char * s)
     return strndup(s, l);
 }
 
-int execute_command(char *command, int input, int output)
+int execute_command(struct Command * cmd)
 {
-	char *argv[MAX_ARGS];              //user command
+	char *argv[MAX_ARGS];
     int  argc;
     char *token;
 	int	status;
-	pid_t	pid, wpid;
+	pid_t pid;
 	
 	/* tokenize and put to array argv[]
 	 * argv[0] will contain the command name */
-	token = strtok(command, " ");
+	token = strtok(cmd->command, " ");
 	argc = 0;
 	while(token != NULL){	
 		argv[argc] = trim(token);
@@ -88,14 +95,14 @@ int execute_command(char *command, int input, int output)
 			*/
 			
 			// redirect stdin and stdout
-			if (input != STDIN_FILENO){
-				dup2(input, STDIN_FILENO);
-				close(input);
+			if (cmd->input != STDIN_FILENO){
+				dup2(cmd->input, STDIN_FILENO);
+				close(cmd->input);
 			}
 			
-			if (output != STDOUT_FILENO){
-				dup2(output, STDOUT_FILENO);
-				close(output);
+			if (cmd->output != STDOUT_FILENO){
+				dup2(cmd->output, STDOUT_FILENO);
+				close(cmd->output);
 			}
 			
 			// execute the command and handle the errors  			
@@ -106,10 +113,76 @@ int execute_command(char *command, int input, int output)
 	}
 	// wait until the child process terminates	
 	do {
-		wpid = waitpid(pid, &status, WUNTRACED);
+		waitpid(pid, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     
 	return status;
+}
+
+struct Command * parse_command(char * command){
+	
+	char * token;
+	int argc;
+	char * argv[2];
+	struct Command * cmd;
+    
+    if((cmd = malloc(sizeof(struct Command))) == NULL){
+		perror("Memory allocation error");
+		return cmd;
+	}
+    
+    cmd->command = command;
+    cmd->input = STDIN_FILENO;
+    cmd->output = STDOUT_FILENO;
+    cmd->output = EXIT_FAILURE;
+    
+	if (strchr(cmd->command, '>') != NULL){
+  		// redirect output
+		token = strtok(cmd->command, ">");
+		argc = 0;
+			
+		while(token != NULL){
+			if (argc > 1){
+				perror("Invalid syntax");
+				return cmd;
+			}	
+			argv[argc] = trim(token);
+			token = strtok(NULL, ">");
+			argc++;
+		}
+		
+		cmd->command = argv[0];
+		cmd->output = open(argv[1], O_WRONLY | O_TRUNC | O_CREAT,  S_IRUSR | S_IWUSR);
+		if (cmd->output == -1){
+			perror("Cannot open the file for writing");
+			return cmd;
+		}
+	}
+						
+	if (strchr(cmd->command, '<') != NULL){
+  		// redirect input 
+		token = strtok(cmd->command, "<");
+		argc = 0;
+			
+		while(token != NULL){
+			if (argc > 1){
+				perror("Invalid syntax");
+				return cmd;
+			}	
+			argv[argc] = trim(token);
+			token = strtok(NULL, "<");
+			argc++;
+		}
+		
+		cmd->command = argv[0];
+		cmd->input = open(argv[1], O_RDONLY);
+		if (cmd->input == -1){		
+			perror("Cannot open the file for reading");
+			return cmd;
+		}			
+  	}
+  	cmd->status = EXIT_SUCCESS;
+  	return cmd;
 }
 
 int main (void) 
@@ -117,10 +190,9 @@ int main (void)
 
 	char	buf[MAXLINE];
 	ssize_t ret;
-	char *argv[MAX_ARGS];
-    int  argc;
-    char *token;
-
+	struct Command *cmd;
+	
+	
 	fprintf(stdout, PROMPT);
 	fflush(stdout);
   	/* Read commands from standard input */
@@ -130,58 +202,16 @@ int main (void)
 		if (buf[ret - 1] == LINE_FEED)
 			buf[ret - 1] = CHAR_NUL;
 
-  		if (strchr(buf, '>') != NULL){
-  			// redirect output
-			token = strtok(buf, ">");
-			argc = 0;
-			
-			while(token != NULL){	
-				argv[argc] = trim(token);
-				token = strtok(NULL, ">");
-				argc++;
-			}
-			if (argc != 2){
-				perror("Invalid command");
-				continue;
-			}
-
-			int output = open(argv[1], O_WRONLY | O_TRUNC | O_CREAT,  S_IRUSR | S_IWUSR);
-			if (output == -1){		
-				perror("Cannot open the file for writing");
-				continue;
-			}
-			execute_command(argv[0], STDIN_FILENO, output);
-			
-		}else if (strchr(buf, '<') != NULL){
-  			// redirect input 
-			token = strtok(buf, "<");
-			argc = 0;
-			
-			while(token != NULL){	
-				argv[argc] = trim(token);
-				token = strtok(NULL, "<");
-				argc++;
-			}
-			if (argc != 2){
-				perror("Invalid command");
-				continue;
-			}
-
-			int input = open(argv[1], O_RDONLY);
-			if (input == -1){		
-				perror("Cannot open the file for reading");
-				continue;
-			}
-			execute_command(argv[0], input, STDOUT_FILENO);
-			
-  		}else{
-  			// execute normal command
-  			execute_command(buf, STDIN_FILENO, STDOUT_FILENO);
-  		}
-  
-  			
+		// execute normal command
+		cmd = parse_command(buf);
+		if (cmd==NULL){
+			perror("Unable to parse the command");
+			continue;
+		}
+		execute_command(cmd);
 		fprintf(stdout, PROMPT);
 		fflush(stdout);
+			
 	}	
   	fprintf (stdout, "Bye!\n");
   	return 0;
