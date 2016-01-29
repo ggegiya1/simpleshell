971c54c15437fdb346f638120ce1511dddb3cc02
Cordeleanu Corneliu <cordeleanu@gmail.com>
2016-01-28 22:41:16 -0500

    rapport

--- ch.c
+++ ch.c
@@ -34,128 +34,163 @@
 #define LINE_FEED		10		// Line feed
 #define CHAR_NUL		0		// Null character
 
-#define MAXLINE			4096		// The maximum length of a command
-#define MAXARGS			4096		// Maximal number of arguments
+#define MAXLINE			4096	// The maximum length of a command
+#define MAXARGS			4096	// Maximal number of arguments
 #define MAXCMDS			128		// Maximal number of commands in the line
 
 #define CURRENT_DIR		"."
 #define PROMPT 			"%%"
 
-/* structure to handle one command */ 
-struct Command{
+/* Structure to handle one command */ 
+
+struct Command {
+
 	char * command;
 	int input;
 	int output;
 	int status;
 };
 
-/* trim extra spaces at the beginning and at the end of command */
-char * trim(char * s) 
-{
+/* Trim extra spaces at the beginning and at the end of command */
+
+char * trim(char * s) {
+
     int l = strlen(s);
+
     while(isspace(s[l - 1])) --l;
     while(* s && isspace(* s)) ++s, --l;
+
     return strndup(s, l);
 }
 
-void read_directory(char * path, char ** files){
-	
+void read_directory(char * path, char ** files) {	
+
 	DIR *dpath = NULL;
 	struct dirent *direntry = NULL;
 	
-	if ((dpath = opendir(path))==NULL){
+	if ((dpath = opendir(path)) == NULL) {
 		perror("Failed to open the directory");
 	}
+
 	int i;
-	for(i=0;(direntry = readdir(dpath)) != NULL;i++){
+	for(i=0; (direntry = readdir(dpath)) != NULL; i++) {
 		files[i] = direntry->d_name;
 	}
+
 	files[i] = NULL;
 }
 
-/* spawn one command */
-int execute_command(struct Command * cmd)
-{
-	char	*argv[MAXARGS];;
-    	int	argc;
-    	char	*token;
-	int	status;
-	pid_t	pid;
-	char    *files[MAXARGS];
+/* Spawn one command */
 
-	/* tokenize and put to array argv[]
-	 * argv[0] will contain the command name */
+int execute_command(struct Command * cmd) {
+
+	char *argv[MAXARGS];
+	char *files[MAXARGS];
+    char *token;
+
+    int	argc;
+	int	status;
+
+	pid_t	pid;
+
+	/* Tokenize and put to array argv[]
+	 * argv[0] will contain the command name 
+	 */
+
 	token = strtok(cmd->command, " ");
-	argc = 0;
-	while(token != NULL){
-		char * argument = trim(token);
-		/* replace the "*' by the list of the files in the current directory */
-		if (strcmp("*", argument) == 0){
+	argc  = 0;
+
+	while(token != NULL) {
+
+		char *argument = trim(token);
+		
+		/* Replace the "*' by the list of the files in the current directory */
+
+		if (strcmp("*", argument) == 0) {
+
 			read_directory(CURRENT_DIR, files);
 			int i;
-			for(i=0;files[i]!=NULL;i++){
+
+			for(i = 0; files[i] != NULL; i++) {
+
 				argv[argc] = files[i];
 				argc++;
+
 				if (argc == MAXARGS){
 					perror("Too many files");
 					return EXIT_FAILURE;
 				}
 			}
-		}else{
+
+		} else {
+
 			argv[argc] = argument;
 			argc++;
+
 			if (argc == MAXARGS){
 				perror("Too many arguments");
 				return EXIT_FAILURE;
 			}
 		}
+
 		token = strtok(NULL, " ");
 	}
+
 	/* NULL is the special element, end of the list */
 	argv[argc] = NULL;
 
-	/* fork the porcess to execute the command */
-	switch (pid = fork()){
+	/* Fork the porcess to execute the command */
+
+	switch (pid = fork()) {
+
 		case -1:
 			perror("Cannot fork");
 			return EXIT_FAILURE;
-		case 0:
-			/*  this is a child code */ 
+			 
+		case 0:	
+			/*  This is a child code */
 	
-			/* redirect input to read from file */
+			/* Redirect input to read from file */
+
 			if (cmd->input != STDIN_FILENO){
 				dup2(cmd->input, STDIN_FILENO);
 				close(cmd->input);
 			}
 			
-			/* redirect output to file */
+			/* Rredirect output to file */
+
 			if (cmd->output != STDOUT_FILENO){
 				dup2(cmd->output, STDOUT_FILENO);
 				close(cmd->output);
 			}
 
-			/* execute the command and handle the errors */
+			/* Execute the command and handle the errors */
+
 			if (execvp(argv[0],argv)<0){
 				perror("Invalid command");
 				return EXIT_FAILURE;
 			}
 	}
 	
-	/* wait until the child process terminates */	
 	do {
+
+		/* Wait until the child process terminates */	
 		waitpid(pid, &status, WUNTRACED);
+
 	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
 	return EXIT_SUCCESS;
 }
 
-struct Command * parse_command(char * command){
+struct Command * parse_command(char * command) {
 	
 	char * token;
-	int argc;
 	char * argv[2];
+	int argc;
+
 	struct Command * cmd;
 
-    	if((cmd = malloc(sizeof(struct Command))) == NULL){
+    if ((cmd = malloc(sizeof(struct Command))) == NULL) {
 		perror("Memory allocation error");
 		return cmd;
 	}
@@ -165,56 +200,68 @@ struct Command * parse_command(char * command){
 	cmd->output = STDOUT_FILENO;
 	cmd->output = EXIT_FAILURE;
     
-	if (strchr(cmd->command, '>') != NULL){
-  		/* redirect output to file */
+	if (strchr(cmd->command, '>') != NULL) {
+  		
+		/* Redirect output to file */
 		token = strtok(cmd->command, ">");
-		argc = 0;		
-		while(token != NULL){
-			if (argc > 1){
+
+		argc  = 0;	
+	
+		while(token != NULL) {
+
+			if (argc > 1) {
 				perror("Invalid syntax");
 				return cmd;
 			}	
+
 			argv[argc] = trim(token);
 			token = strtok(NULL, ">");
 			argc++;
 		}
 		
 		cmd->command = argv[0];
-		cmd->output = open(argv[1], O_WRONLY | O_TRUNC | O_CREAT,  S_IRUSR | S_IWUSR);
-		if (cmd->output == -1){
+		cmd->output  = 
+			open(argv[1], O_WRONLY | O_TRUNC | O_CREAT,  S_IRUSR | S_IWUSR);
+
+		if (cmd->output == -1) {
 			perror("Cannot open the file for writing");
 			return cmd;
 		}
 	}
 						
-	if (strchr(cmd->command, '<') != NULL){
-  		/* read the input from file */ 
+	if (strchr(cmd->command, '<') != NULL) {
+  		
+		/* Read the input from file */ 
 		token = strtok(cmd->command, "<");
+
 		argc = 0;
 			
-		while(token != NULL){
-			if (argc > 1){
+		while(token != NULL) {
+
+			if (argc > 1) {
 				perror("Invalid syntax");
 				return cmd;
 			}	
+
 			argv[argc] = trim(token);
 			token = strtok(NULL, "<");
 			argc++;
 		}
 		
 		cmd->command = argv[0];
-		cmd->input = open(argv[1], O_RDONLY);
-		if (cmd->input == -1){		
+		cmd->input   = open(argv[1], O_RDONLY);
+
+		if (cmd->input == -1) {		
 			perror("Cannot open the file for reading");
 			return cmd;
 		}			
   	}
+
   	cmd->status = EXIT_SUCCESS;
   	return cmd;
 }
 
-int main (void) 
-{
+int main (void) {
 
 	char	buf[MAXLINE];
 	ssize_t ret;
@@ -227,78 +274,105 @@ int main (void)
 	
 	fprintf(stdout, PROMPT);
 	fflush(stdout);
+
   	/* Read commands from standard input */
+
 	while ((ret = read(STDIN_FILENO, buf, MAXLINE)) > 0) {
-		status = EXIT_SUCCESS;
 		
-		/* terminate the string by 0*/
+		status  = EXIT_SUCCESS;
+		
+		/* Terminate the string by 0*/
+
 		if (buf[ret - 1] == LINE_FEED)
 			buf[ret - 1] = CHAR_NUL;
 
-		/* allow using pipes */
+		/* Allow using pipes */
+
 		if (strchr(buf, '|') != NULL){
 
-			/* parse commamnds and store in cmd[] array */
+			/* Parse commamnds and store in cmd[] array */
+
 			char * token = strtok(buf, "|");
 			input = STDIN_FILENO;
 			count = 0;
-			while(token != NULL){
-				struct Command * one_command = parse_command (token);
-				if (one_command==NULL || one_command->status==EXIT_FAILURE){
+
+			while(token != NULL) {
+
+				struct Command * _command = parse_command (token);
+				
+				if (_command == NULL || _command->status == EXIT_FAILURE) {
+					
 					perror("Unable to parse the command");
 					status = EXIT_FAILURE;
 					break;
 				}
-				cmd[count++] = one_command;
+
+				cmd[count++] = _command;
 				token = strtok(NULL, "|");
-				if (count == MAXCMDS){
+
+				if (count == MAXCMDS) {
 					perror("Too many commands");
 					status = EXIT_FAILURE;
 					break;
 				}
 			}
 			
-			/* execute commands in chain if the command line was successfully parsed */
-			if (status == EXIT_SUCCESS){
+			/* Execute commands in chain if the command line was successfully parsed */
+
+			if (status == EXIT_SUCCESS) {
+				
 				int i;
-				/* execute commands secuentially */
-				for (i=0; i<count-1; i++){
+
+				/* Execute commands secuentially */
+
+				for (i=0; i < count - 1; i++){
 					
-					/* create pipe */
+					/* Create pipe */
 					pipe(_pipe);
 					
-					/* connect input and output to the pipe */  
-					cmd[i]->input = input;
+					/* Connect input and output to the pipe */ 
+
+					cmd[i]->input  = input;
 					cmd[i]->output = _pipe[1];
 					
 					execute_command (cmd[i]);
-					/* close output , but keep input for the next command */
+
+					/* Close output , but keep input for the next command */
+
 					close(_pipe[1]);
 					input = _pipe[0];
 					free(cmd[i]);
 				}
-				/* last command reads from pipe but writes to STDOUT */
+
+				/* Last command reads from pipe but writes to STDOUT */
+
 				cmd[i]->input = input;
 				execute_command (cmd[i]);
 				close(_pipe[0]);
 				free(cmd[i]);
 			}
+
 		}else{
-			/* execute one command */
-			struct Command * one_command = parse_command(buf);
-			if (one_command == NULL || one_command->status == EXIT_FAILURE){
+
+			/* Execute one command */
+			struct Command * _command = parse_command(buf);
+
+			if (_command == NULL || _command->status == EXIT_FAILURE){
 				perror("Unable to parse the command");
 				continue;
 			}
-			execute_command(one_command);
-			free(one_command);
+
+			execute_command(_command);
+			free(_command);
 		}
 		
 		fprintf(stdout, PROMPT);
 		fflush(stdout);
 			
 	}	
+
 	fflush(stdout);
   	fprintf (stdout, "Bye!\n");
+
   	return 0;
 }
